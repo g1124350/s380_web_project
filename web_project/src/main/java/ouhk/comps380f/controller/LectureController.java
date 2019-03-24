8be2530f70d4c0450ac2d2d85c8f2c69968e446b
g1124350 <45535179+g1124350@users.noreply.github.com>
2019-03-24 12:25:30 +0800

    Add files via upload

---
+++ web_project/src/main/java/ouhk/comps380f/controller/LectureController.java
@@ -0,0 +1,24 @@
+package ouhk.comps380f.controller;
+
+import java.util.Hashtable;
+import java.util.Map;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import ouhk.comps380f.model.LectureModel;
+
+@Controller
+@RequestMapping("lecture")
+public class LectureController {
+
+    private volatile long LECTURE_ID_SEQUENCE = 1;
+    private Map<Long, LectureModel> lectureDatabase = new Hashtable<>();
+
+//    @RequestMapping(value = {"", "list"}, method = RequestMethod.GET)
+    public String list(ModelMap model) {
+        model.addAttribute("lectureDatabase", lectureDatabase);
+        return "list";
+    }
+  
+}
